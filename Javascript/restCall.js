77388292e9bcf9ddee9f5e9678c710a560576dbf
Christoph <ck120@hdm-stuttgart.de>
2017-01-19 12:59:05 +0100

    refactor adaptive gmap

--- Javascript/restCall.js
+++ Javascript/restCall.js
@@ -24,16 +24,46 @@ window.onload = function loadDoc() {
         if (this.readyState == 4 && this.status == 200) {
             var jsonObj = JSON.parse(xhttpreq.responseText);
             console.log(jsonObj);
+            jsonObjPersonOne = [
+                {
+                    "more-contrast": "true",
+                    "text-alternatives": "text",
+                    "font-size": "l",
+                    "reading-direction": "ltr"
+
+                }
+            ];
+            jsonObjPersonTwo = [
+                {
+                    "more-contrast": "true",
+                    "text-alternatives": "text",
+                    "font-style": "serif",
+                    "font-size": "l",
+                    "reading-direction": "rtl"
+
+                }
+            ];
+            jsonObjPersonThree = [
+                {
+                    "more-contrast": "true",
+                    "text-alternatives": "text",
+                    "font-style": "serif",
+                    "font-size": "l",
+                    "reading-direction": "rtl"
+
+                }
+            ];
+            jsonObj = jsonObjPersonOne;
             var adaptiveElements = document.querySelectorAll('.adaptive-polymer-element');
             for (var i = 0; i < adaptiveElements.length; i++) {
                 for (var prop in jsonObj[0]) {
                     adaptiveElements[i].setAttribute(prop, jsonObj[0][prop]);
                 }
-                adaptiveElements[i]._adaptive();
+                adaptiveElements[i].adaptive();
             }
         }
     };
-    xhttpreq.open("GET", 'https://raw.githubusercontent.com/christophkleber/data/master/person-one.json', true);
+    xhttpreq.open("GET", 'https://raw.githubusercontent.com/christophkleber/data/master/person-two.json', true);
     xhttpreq.send();
 
 };
